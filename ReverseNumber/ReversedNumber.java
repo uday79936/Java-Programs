0dd96c78e5f8c01d988af60a9b33b98da6db512b
Akhil Kommineni <akhilkommineni6598@gmail.com>
2026-03-23 11:46:33 +0530

    added

---
+++ ReverseNumber/ReversedNumber.java
@@ -0,0 +1,19 @@
+package ReverseNumber;
+
+public class ReversedNumber{
+    public static void main(String[] args) {
+        int number = 12345;
+
+        int reversed = 0;
+
+        while(number != 0)
+        {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+
+            number = number / 10;
+        }
+            System.out.println("Reverse Number is: "+ reversed);
+
+    }
+}
