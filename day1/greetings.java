a41a40f2ba5bc9ffcb00ab9ba4922b917cef412f
Akhil Kommineni <akhilkommineni6598@gmail.com>
2026-03-24 15:14:23 +0530

    added

---
+++ day1/greetings.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class greetings {
+    public static void main(String[] args) {
+        String name="Uday";
+
+        System.out.println("Good Morning "+ name);
+
+    }
+}
