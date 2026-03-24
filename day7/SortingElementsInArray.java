85ddd26d2a83f9d5375c713eee695f4aaa04715c
Akhil Kommineni <akhilkommineni6598@gmail.com>
2026-03-24 14:44:59 +0530

    added

---
+++ day7/SortingElementsInArray.java
@@ -0,0 +1,15 @@
+package day7;
+
+import java.util.Arrays;
+
+public class SortingElementsInArray {
+    public static void main(String[] args) {
+      
+        int a[] = {10,30,20,50,40};
+
+        Arrays.sort(a);
+
+        System.out.println(Arrays.toString(a));
+
+    }
+}
