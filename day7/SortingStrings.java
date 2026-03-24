23786d7e0599ff795518872cc11774d43d274413
Akhil Kommineni <akhilkommineni6598@gmail.com>
2026-03-24 14:59:42 +0530

    added

---
+++ day7/SortingStrings.java
@@ -0,0 +1,14 @@
+package day7;
+
+import java.util.Arrays;
+
+public class SortingStrings {
+    public static void main(String[] args){
+        String s[] = {"Scott", "Marry", "John", "David"};
+
+        Arrays.sort(s);
+
+        System.out.println(Arrays.toString(s));
+    }
+    
+}
